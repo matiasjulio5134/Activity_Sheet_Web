0b6a5683b18f05ac800747197cc0d9bb9b1bcf29
Antonio Navarro <dojeDulDev@Proton.me>
2026-09-08 12:15:34 +0200

    fix-general-01: No mostrar informacion sensible en console.log

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -106,7 +106,7 @@ function Login() {
       const resp = await axiosInstance.post("/users/login", userData);
 
       // Mostar la respuesta por consola
-      console.log("RESPUESTA LOGIN:", resp.data);
+      //console.log("RESPUESTA LOGIN:", resp.data);
 
       // Guardar los datos del usuario
       localStorage.setItem("usuario", JSON.stringify(resp.data));
